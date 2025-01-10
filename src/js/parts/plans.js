abb804e4c26737a1d8f79d352b423c527ee1f5c1
Uzurch1k <galcenkovlad7@gmail.com>
2025-01-10 19:50:02 +0200

    fix

--- src/js/parts/plans.js
+++ src/js/parts/plans.js
@@ -7,21 +7,14 @@ const plansSix = document.querySelector('.plansSix');
 periodInputs?.forEach(input => {
   input.addEventListener('change', () => {
     plans.forEach(plan => {
-      plan.classList.remove('is-visible');
       plan.classList.add('is-hidden');
     });
 
     if (input.value === '3') {
       plansThree.classList.remove('is-hidden');
-      setTimeout(() => {
-        plansThree.classList.add('is-visible');
-      }, 100);
     }
     if (input.value === '6') {
       plansSix.classList.remove('is-hidden');
-      setTimeout(() => {
-        plansSix.classList.add('is-visible');
-      }, 100);
     }
   });
 });
