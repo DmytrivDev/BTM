a93df2b6aadc06ac8e90947b3d2df70d2ee2faeb
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-01-20 13:00:24 +0200

    іаів

--- src/js/parts/select.js
+++ src/js/parts/select.js
@@ -1,9 +1,23 @@
 import NiceSelect from 'nice-select2';
 
-const eventsSelect = document.querySelectorAll('.events__select select');
+const eventsSelectCh = document.querySelector('.events__select');
 
-eventsSelect.forEach(select => {
-  new NiceSelect(select, {
-    searchable: false,
+if (eventsSelectCh) {
+  document.addEventListener('facetwp-loaded', function () {
+    const eventsSelect = document.querySelectorAll('.events__select select');
+    const allTxt = eventsSelectCh.dataset.all;
+
+    eventsSelect.forEach(select => {
+      if (!select.nextElementSibling || !select.nextElementSibling.classList.contains('nice-select')) {
+        const cont = select.closest('.facetwp-facet');
+        const name = cont.dataset.name;
+        const title = eventsSelectCh.dataset[name];
+        cont.insertAdjacentHTML('afterbegin', '<p>'+title+'</p>')
+        new NiceSelect(select, {
+          searchable: false,
+          placeholder: allTxt
+        });
+      }
+    });
   });
-});
+}
