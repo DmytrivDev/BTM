c8c024543b6c508eed0507219a15f33816a3e650
Uzurch1k <galcenkovlad7@gmail.com>
2025-05-09 15:24:09 +0300

    fix

--- src/js/parts/ceo.js
+++ src/js/parts/ceo.js
@@ -1,13 +1,13 @@
 export function addDataCeo(modal, event) {
-  const ceo = event.currentTarget.closest('.ceo');
+  const ceoItem = event.currentTarget.closest('.ceo__item');
 
-  if (!ceo) return;
+  if (!ceoItem) return;
 
   const ceoData = {
-    title: ceo.dataset.title || '',
-    descript: ceo.dataset.descript || '',
+    title: ceoItem.querySelector('.tl4')?.textContent || '',
+    descript: ceoItem.querySelector('.ceo__descript')?.innerHTML || '',
   };
 
   modal.querySelector('.ceoinf__tl').textContent = ceoData.title;
-  modal.querySelector('.ceoinf__txt p').textContent = ceoData.descript;
+  modal.querySelector('.ceoinf__txt').innerHTML = ceoData.descript;
 }
